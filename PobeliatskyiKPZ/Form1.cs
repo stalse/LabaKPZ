393d4c8ecb6358a9ada8a4908f21afa669a294c3
stalse <95195513+stalse@users.noreply.github.com>
2021-12-01 00:01:34 +0200

    Add files via upload

--- PobeliatskyiKPZ/Form1.cs
+++ PobeliatskyiKPZ/Form1.cs
@@ -12,20 +12,57 @@ namespace PobeliatskyiKPZ
 {
     public partial class Form1 : Form
     {
+        int a, b, c, d, e, f, g;
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                listBox1.Items.Add(Convert.ToInt32(Convert.ToString(comboBox1.SelectedItem)));
+            }
+            catch
+            {
+
+            }
+        }
+
         public Form1()
         {
             InitializeComponent();
         }
-
-        private void button1_Click(object sender, EventArgs e)
+        void CreatePicture(Control Parent, Point Pos)
         {
-            string a = textBox1.Text;
-            listBox1.Items.Add(a);
+            PictureBox pct = new PictureBox();
+            pct.BackColor = Color.White;
+            pct.Location = Pos;
+            pct.Size = new Size(150, 70);
+            pct.Text = listBox1.Text;
+            Parent.Controls.Add(pct); 
         }
 
-        private void textBox3_TextChanged(object sender, EventArgs e)
+        int i = -70;
+        private void button1_Click(object sender, EventArgs e)
         {
-            
+            try
+            {
+                listBox1.Items.Add(Convert.ToInt32(textBox1.Text));
+            }
+            catch
+            {
+                MessageBox.Show("Це не число, введіть будь-ласка число");
+                textBox1.Clear();
+            }
+            i += 80;
+            CreatePicture(this, new Point(500, i));    
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
